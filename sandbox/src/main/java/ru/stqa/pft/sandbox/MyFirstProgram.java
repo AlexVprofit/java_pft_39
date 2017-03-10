b1207c35ab5939e8ec9c95f09ad940beed89b70d
AlexVprofit <vprofit79@gmail.com>
2017-03-11 01:22:18 +0300

    Added several fanction

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
- public static void main(String[] args){
-	System.out.println("Hello World!");
+ public static void main(String[] args) {
+   hello("World");
+   hello("user");
+   hello("Alexey");
+
+   double l = 8;
+   System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+   double a = 4;
+   double b = 6;
+   System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +  " = " + area(a,b));
  }
+	public static void hello(String somebody) {
+    //String somebody = "World";
+    System.out.println("Hello, " +somebody+ " !");
+  }
+  public static double area(double l) {
+    return l * l;
+  }
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
 }
