fbb3b81585b935b7a09a5451074ea8ee51365e3b
AlexVprofit <vprofit79@gmail.com>
2017-03-18 23:54:23 +0300

    Number 9, Created delegation and aixiliary classes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmeneger/ApplicationMeneger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmeneger/ApplicationMeneger.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.appmeneger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
@@ -78,5 +79,62 @@ public class ApplicationMeneger {
 
   public void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
+  }
+
+  public void refreshHomePage() {
+    wd.findElement(By.name("searchstring")).click();
+    wd.findElement(By.name("searchstring")).clear();
+    wd.findElement(By.name("searchstring")).sendKeys();
+  }
+
+  public void returnHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  public void inputAddNewFormContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  public void fillAddNewFormContact(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getNew_adress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getTelhome());
+  }
+
+  public void gotoAddNew() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  public void confirmationDeleteContact() {
+    wd.switchTo().alert().accept();
+  }
+
+  public void deleteStringContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+  }
+
+  public void selectStringContact() {
+    if (!wd.findElement(By.name("selected[]")).isSelected()) {
+      wd.findElement(By.name("selected[]")).click();
+    }
+  }
+
+  public void menuHome() {
+    wd.findElement(By.linkText("home")).click();
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class NewContactCreationTest extends TestBase {
+
+  @Test
+  public void testNewContactCreation() {
+    app.gotoAddNew();
+    app.fillAddNewFormContact(new ContactData("Alex", "Alexbond", "Title", "Education", "new adress", "12345"));
+    app.inputAddNewFormContact();
+    app.returnHomePage();
+    app.refreshHomePage();
+  }
+
+}
