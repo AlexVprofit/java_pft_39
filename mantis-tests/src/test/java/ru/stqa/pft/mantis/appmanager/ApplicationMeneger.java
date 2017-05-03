75a517426a748d7d0d0a45a83e27a1555f3380b8
AlexVprofit <vprofit79@gmail.com>
2017-05-03 13:16:13 +0300

    The template lazy initialization (first metod RegistrationTests, then metod LoginTests)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationMeneger.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationMeneger.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,9 +19,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationMeneger {
 
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationMeneger(String browser) {
     this.browser = browser;
@@ -32,26 +34,49 @@ public class ApplicationMeneger {
     String target = System.getProperty("target", "local");
     // загружаем файл
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (Objects.equals(browser, BrowserType.FIREFOX)) {
-      // потому что зачудил !!!
-     FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe"));
-     wd = new FirefoxDriver(binary, new FirefoxProfile());
-//      wd = new FirefoxDriver();
-      //  это памятка как пример завел
-
-    } else if (Objects.equals(browser, BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (Objects.equals(browser, BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+   if (wd != null) {
+     wd.quit();
+   }
   }
 
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    // если не было инициализации, т.е. только один раз инициализируем при обращении к методу registration()
+    if (registrationHelper == null) {
+       registrationHelper = new RegistrationHelper(this); // менеджер нанимает помшника и передаёт ссылку на самого себя
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() { // драйвер браузера инициализируется если к нему кто-то обратиться через getDriver() {
+    if (wd == null) {
+      if (Objects.equals(browser, BrowserType.FIREFOX)) {
+        // потому что зачудил !!!
+        FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe"));
+        wd = new FirefoxDriver(binary, new FirefoxProfile());
+//      wd = new FirefoxDriver();
+        //  это памятка как пример завел
+
+      } else if (Objects.equals(browser, BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (Objects.equals(browser, BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+
+    }
+    return wd;
+  }
 }
